91fbc1743bf0a1500565d7e717f9499ca97c64ee
Mirosław Błażej <miro662@gmail.com>
2016-12-21 22:51:49 +0100

    Added periodical temperature checks

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 
 var cpuTempSensorFile = "/sys/class/thermal/thermal_zone0/temp"
+var checkInterval = 2000;
 
 function getCpuTemperature(callback) {
 	fs.readFile(cpuTempSensorFile, function(err, data) {
@@ -12,6 +13,15 @@ function getCpuTemperature(callback) {
 	});
 }
 
-getCpuTemperature(function(err, data) {
-	console.log(data);
-});
+var cpuTemperature = NaN;
+
+setInterval(function() {
+	getCpuTemperature(function(err, data) {
+		if (err) {
+			cpuTemperature = NaN;
+			return console.log(err);
+		}
+		cpuTemperature = data;
+		console.log(cpuTemperature);
+	});
+})
